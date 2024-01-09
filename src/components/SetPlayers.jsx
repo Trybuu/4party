0fbb324e085050d734506542a70330fbe217b808
Marek <trybu001@gmail.com>
2024-01-09 17:47:37 +0100

    Ready button

--- src/components/SetPlayers.jsx
+++ src/components/SetPlayers.jsx
@@ -1,21 +1,16 @@
 import { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
 import classes from './SetPlayers.module.scss'
 import Input from '../ui/inputs/Input'
+import Button from '../ui/buttons/Button'
 import ErrorMessage from '../ui/error/ErrorMessage'
 
 export default function SetPlayers() {
-  const [players, setPlayers] = useState({
-    // Marek: {
-    //   id: 0,
-    //   name: 'Marek',
-    //   score: 0,
-    // },
-    // Julka: {
-    //   id: 1,
-    //   name: 'Julka',
-    //   score: 0,
-    // },
-  })
+  const [players, setPlayers] = useState({})
+
+  // Dodać możliwość edytowania graczy po ich dodaniu
+  // Dodać możliwość usunięcia gracza
+  // Zapisać graczy w globalnym stanie i w localStorage
 
   const [isPlayerExist, setIsPlayerExist] = useState(false)
 
@@ -45,13 +40,10 @@ export default function SetPlayers() {
       input.current.value = ''
       input.current.focus()
     }
+  }
 
-    const playerValues = Object.values(players)
-    console.log(playerValues)
-
-    playerValues.forEach((player) => {
-      console.log(`${player.name} ma wynik ${player.score} i id ${player.id}`)
-    })
+  function handlePlayersReady() {
+    console.log('READY')
   }
 
   return (
@@ -84,78 +76,12 @@ export default function SetPlayers() {
           </Input>
         ))}
       </div>
+
+      <div className={classes.ready}>
+        {Object.values(players).length >= 2 ? (
+          <Button onClick={handlePlayersReady}>Gotowi</Button>
+        ) : undefined}
+      </div>
     </div>
   )
 }
-
-// import { useRef, useState } from 'react'
-// import classes from './SetPlayers.module.scss'
-// import Input from '../ui/inputs/Input'
-// import ErrorMessage from '../ui/error/ErrorMessage'
-
-// export default function SetPlayers() {
-//   let players = [
-//     {
-//       id: 87429387,
-//       name: 'Marek',
-//       score: 0,
-//     },
-//     {
-//       id: 87429322,
-//       name: 'Julka',
-//       score: 0,
-//     },
-//   ]
-
-//   const [isPlayerExist, setIsPlayerExist] = useState(false)
-
-//   const input = useRef()
-
-//   function handleAddPlayer() {
-//     const playerName = input.current.value
-
-//     // Chceck if player name exist
-//     const isExist = players.filter((player) => player.name === playerName)
-
-//     if (isExist.length > 0) {
-//       setIsPlayerExist(true)
-//       input.current.value = ''
-//       input.current.focus()
-//       return
-//     } else {
-//       setIsPlayerExist(false)
-
-//       const player = {
-//         id: Math.floor(Math.random() * 1000000),
-//         name: playerName,
-//         score: 0,
-//       }
-
-//       players.push(player)
-
-//       input.current.value = ''
-//       input.current.focus()
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <Input>
-//         <input
-//           ref={input}
-//           type="text"
-//           placeholder="Nazwa gracza"
-//           maxLength={16}
-//         />
-//         <button onClick={handleAddPlayer}>+</button>
-//       </Input>
-//       <ErrorMessage
-//         error={
-//           isPlayerExist === true
-//             ? 'Gracz o podanej nazwie już istnieje!'
-//             : undefined
-//         }
-//       />
-//     </div>
-//   )
-// }
